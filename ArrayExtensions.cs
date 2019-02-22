32f9e09a14e51bc8d156a1fa848598fa03088aed
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-22 22:11:35 +0300

    1

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -1,13 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PadawansTask11
 {
     public static class ArrayExtensions
     {
+        private static double GetSum(double[] arr)
+        {
+            double sum = 0;
+            foreach (double var in arr)
+                sum += var;
+            return sum;
+        }
+
+        private static double[] GetSubArray(double[] arr, int start, int end)
+        {
+            List<double> l = new List<double>();
+            for (int i = start; i <= end; i++)
+                l.Add(arr[i]);
+
+            return l.ToArray();
+        }
+
+
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length < 3)
+                throw new ArgumentException();
+            if (accuracy < 0 || accuracy > 1)
+                throw new ArgumentException();
+
+            double[] subarrLeft;
+            double[] subarrRight;
+
+            double sum1 = 0, sum2 = 0;
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                subarrLeft = GetSubArray(array, 0, i - 1);
+                subarrRight = GetSubArray(array, i + 1, array.Length - 1);
+                sum1 = GetSum(subarrLeft);
+                sum2 = GetSum(subarrRight);
+                if (Math.Abs(sum1 - sum2) <= accuracy)
+                    return i;
+            }
+
+            return null;
         }
     }
 }
